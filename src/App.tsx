446e27d068ceeecc469d5cee72c46273638ffb88
Joel George V <joel.gv3@gmail.com>
2026-02-27 15:27:33 +0000

    refactor: remove styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
-import styled from 'styled-components'
+import './App.css'
 
 import github from './assets/github.svg'
 
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 80%;
-  max-width: 64rem;
-  height: 100vh;
-  margin: 0 auto;
-`
-
-const Header = styled.header`
-  text-align: center;
-`
-
-const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  overflow: auto;
-`
-
-const Footer = styled.footer`
-  margin: 2rem;
-`
-
 export const App = () => (
-  <Main>
-    <Header>
+  <main className='main'>
+    <header className='header'>
       <h1>Game of Life</h1>
-    </Header>
-    <Section>
+    </header>
+    <section className='section'>
       <article></article>
-    </Section>
-    <Footer>
+    </section>
+    <footer className='footer'>
       <a href='https://github.com/joelgeorgev/game-of-life'>
         <img src={github} alt='Go to GitHub repository page' />
       </a>
-    </Footer>
-  </Main>
+    </footer>
+  </main>
 )
